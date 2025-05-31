d52ea256f6fa23b92a6ae702dc4b44ad550384d7
Darrylene <dk.aranzasu@gmail.com>
2025-05-31 16:35:59 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './TodoList'; // Import your TodoList component
+import './App.css'; // You can add some basic styling here
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
